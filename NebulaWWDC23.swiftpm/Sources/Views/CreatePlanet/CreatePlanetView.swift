82c35cf8b591eb74b6e7e90776c8af9e16792018
CynaraCosta <cvocac@cin.ufpe.br>
2023-04-11 23:47:11 -0300

    created the createPlanetView but need to implement the func to add indeed a planet

--- NebulaWWDC23.swiftpm/Sources/Views/CreatePlanet/CreatePlanetView.swift
+++ NebulaWWDC23.swiftpm/Sources/Views/CreatePlanet/CreatePlanetView.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import Combine
+
 
 struct CreatePlanetView: View {
     @State private var namePlanet: String = ""
     @State private var gravityPlanet: String = ""
     @State private var currentIndex: Int = 0
-//    let images = ["frame-gravity-earth-1", "frame-gravity-earth-2", "frame-gravity-earth-3"]
+    
+    @State private var validateFields = true
+    
     let images = ["sample-planet"]
     
     var body: some View {
@@ -13,13 +17,13 @@ struct CreatePlanetView: View {
                 .resizable()
                 .ignoresSafeArea(.all)
             
-            VStack (spacing: 100) {
+            VStack (spacing: 72) {
                 
                 HStack (spacing: 64) {
                     
                     Button(action: {
                         withAnimation {
-                            // Ação para a seta da esquerda com animação
+                            
                             currentIndex = (currentIndex - 1 + images.count) % images.count
                         }
                     }) {
@@ -29,13 +33,11 @@ struct CreatePlanetView: View {
                     Image(images[currentIndex])
                         .resizable()
                         .scaledToFit()
-                        .opacity(1) // Define a opacidade da imagem atual como 1 para exibi-la no centro
-                        .animation(.easeInOut(duration: 0.5))
                     
                     
                     Button(action: {
                         withAnimation {
-                            // Ação para a seta da direita com animação
+                            
                             currentIndex = (currentIndex + 1) % images.count
                         }
                     }) {
@@ -65,6 +67,14 @@ struct CreatePlanetView: View {
                             .foregroundColor(.white)
                             .autocapitalization(.none)
                             .disableAutocorrection(true)
+                            .onReceive(Just(namePlanet)) { newValue in
+                                let allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-&*ˆ%$#@!+=';"
+                                let filtered = newValue.filter { allowedCharacters.contains($0) }
+                                if filtered != newValue {
+                                    self.namePlanet = filtered
+                                }
+                                
+                            }
                             .frame(width: UIScreen.getScreenWidth() * 0.77, height: UIScreen.getScreenHeight() * 0.056, alignment: .leading)
                     }
                 }
@@ -85,31 +95,53 @@ struct CreatePlanetView: View {
                                     .stroke(Color.white, lineWidth: 2)
                             )
                             .foregroundColor(.white)
-                            .keyboardType(.numberPad)
+                            .keyboardType(.decimalPad)
                             .autocapitalization(.none)
                             .disableAutocorrection(true)
+                            .onReceive(Just(gravityPlanet)) { newValue in
+                                let allowedCharacters = "0123456789."
+                                let filtered = newValue.filter { allowedCharacters.contains($0) }
+                                if filtered != newValue {
+                                    self.gravityPlanet = filtered
+                                }
+                                
+                            }
                             .frame(width: UIScreen.getScreenWidth() * 0.77, height: UIScreen.getScreenHeight() * 0.056, alignment: .leading)
+                        
                     }
+                    
+                    
+                }
+                
+                if !validateFields {
+                    Text("Please fill in all fields!")
+                        .font(.custom(.vt323, size: 20))
+                        .foregroundColor(Color.red)
+                        .frame(width: UIScreen.getScreenWidth() * 0.77, height: UIScreen.getScreenHeight() * 0.05, alignment: .leading)
                 }
                 
                 Button(action: {
-                    
+                    if gravityPlanet == "" || namePlanet == ""{
+                        validateFields = false
+                    } else {
+                        validateFields = true
+                    }
                 }) {
                     Image.theme.simulateButton
+                        .resizable()
+                        .scaledToFit()
                         .frame(width: UIScreen.getScreenWidth() * 0.34, height: UIScreen.getScreenHeight() * 0.05)
                 }
-
-                
             }
         }
     }
 }
-
 
 struct CreatePlanetView_Previews: PreviewProvider {
     static var previews: some View {
         CreatePlanetView()
     }
 }
+
 
 
