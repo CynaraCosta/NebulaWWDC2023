0e7b88dc6cc298e361dc1b6740e105586c689b33
CynaraCosta <cvocac@cin.ufpe.br>
2023-04-18 14:54:01 -0300

    updated the contents file with some text

--- NebulaWWDC23.swiftpm/Contents.swift
+++ NebulaWWDC23.swiftpm/Contents.swift
@@ -1,2 +1,49 @@
-// used fonts and stuff
+/*
+ 
+ # Nebula
+ 
+ ## How does it work?
+ 
+ Welcome to this interplanetary experience! We are excited to have you on board on this journey across the universe. Here is some information to help you get started
+ 
+ In this experiment we offer you the opportunity to explore information about some planets in our solar system and simulate some physics related to them, such as gravity, collision and impulse.
+ 
+ That is, you can experience the feeling of being on another planet and have a unique and fun experience.
+ 
+ In addition, you can create your own planet and customize your information. You will have the opportunity to experiment and create unique planets that have never existed before!
+ 
+ I hope you have an amazing experience and a good interplanetary trip!
+ 
+ Note that this app is a simulation game, so you can test anything you want, there is no right or wrong!
+ 
+ ## Who am I
+ 
+ Hi! My name is Cynara Costa and I am 21 years old. I am a student in computer science at the UFPE University, Recife - Brazil.
+ 
+ I also participated of a Junior Company named CITi as a Product Designer before joining the Apple Developer Academy in 2022. Before that, I had no contact with anything close to Swift or other techlonogys from apple.
+ 
+ This is my second attempt for the Swift Student Challenge, the first and accepted submission was the "GraviNewton" and in this second, I wanted to challenge more myself.
+ 
+ Because of that I wanted to use and learn things different from the previous attempt, so for this Project, I used Swiftui as well, but inovate with SpriteKit for doing the simulations and SceneKit for implement the 3D model for the planets.
+ 
+ And I feel in the obrigation to say that it was pretty challenge for me to include and understang the principles behind the rotation animation at the 3D model for the planets using SceneKit, since I had to learn about Camera, Rotation, Pivot Point and other things to get that effect at the planets! But when I got the positive result, I felt so Happy!
+ 
+ Also, in terms of code, I was very ansious to learn about Design Patterns for use with Swift, so I implemented the MVVM for this project!
+ 
+ Also, I discovered e new passion for PixelArt and VoxelArt since the last application, so all of the sprites, includind the 3D ones, were made by me and I loved doing it!
+ 
+ 
+ ## Credits
+ 
+ The design for the app was done all by myself, except for the two fonts I used in the project!
+ 
+ Here I leave also a very Thank you for all the softwares that allowed me to create the sprites for this project:
+ - Aseprite: Where I created all the 2D pixel art for this app (https://www.aseprite.org/)
+ - MagicaVoxel: Where I created all the 3D voxel art for this app (https://ephtracy.github.io/)
+ 
+ Also thank to all custom fonts in this project that were not authored by me. So, I leave available here the place where I downloaded them:
+ - VT323: [GoogleFonts] https://fonts.google.com/specimen/VT323?query=VT323
+ - Inconsolata: [GoogleFonts] https://fonts.google.com/specimen/Inconsolata?query=Inconsolat
+ 
+ */
 
